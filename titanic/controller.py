c89920198c9eb34df103d970ea98d693ffb09fb2
pakjkwan <pakjkwan@gmail.com>
2019-06-15 10:20:36 +0900

    10:20

--- titanic/controller.py
+++ titanic/controller.py
@@ -3,7 +3,7 @@ from titanic.model import TitanicModel
 class TitanicController:
     def __init__(self):
         self._m = TitanicModel()
-        self._context = './data'
+        self._context = './data/'
         self._train = self.create_train()
 
     def create_train(self) -> object:
@@ -11,14 +11,21 @@ class TitanicController:
         m.context = self._context
         m.fname = 'train.csv'
         t1 = m.new_dframe()
+        print('---------- train head & column -----------')
+        print(t1.head())
+
         m.fname = 'test.csv'
         t2 = m.new_dframe()
-        train = m.hook_process(t1, t2)
+        print('---------- test head & column -----------')
+        print(t2.head())
+
+        print('---------- hook 메소드 시작 -----------')
+        # train = m.hook_process(t1, t2)
         print('---------- 1 -----------')
-        print(train.column)
+        # print(train.column)
         print('---------- 2 -----------')
-        print(train.head())
-        return train
+        # print(train.head())
+        # return train
 
     def create_model(self):
         pass
--- titanic/model.py
+++ titanic/model.py
@@ -29,31 +29,31 @@ class TitanicModel:
     @property
     def context(self) -> object:return self._context
 
-    @property.setter
+    @context.setter
     def context(self, context): self._context = context
 
     @property
     def fname(self) -> object: return self._fname
 
-    @property.setter
+    @fname.setter
     def fname(self, fname): self._fname = fname
 
     @property
     def train(self) -> object: return self._train
 
-    @property.setter
+    @train.setter
     def train(self, train): self._train = train
 
     @property
     def test(self)-> object: return self._test
 
-    @property.setter
+    @test.setter
     def test(self, test): self._test = test
 
     @property
     def test_id(self)-> object: return self._test_id
 
-    @property.setter
+    @test_id.setter
     def test_id(self, test_id): self._test_id = test_id
 
     def new_file(self) -> str: return self._context + self._fname
@@ -63,12 +63,36 @@ class TitanicModel:
         return pd.read_csv(file)
 
     def hook_process(self, train, test) -> object:
-        t = None
-        
+        print('-------------1. Cabin, Ticket 삭제 ---------------')
+        t = self.drop_feature(train, test, 'Cabin')
+        t = self.drop_feature(t[0], t[1], 'Ticket')
+        print('-------------2. Embarked 편집 --------------------')
+        t = self.embarked_norminal(t[0], t[1])
+        print('-------------3. Title 편집 -----------------------')
+
         return t
 
+    @staticmethod
+    def drop_feature(train,test,feature)->[]:
+        train = train.drop([feature], axis = 1)
+        test = test.drop([feature], axis=1)
+        return [train, test]
 
+    @staticmethod
+    def embarked_norminal(train, test)->[]:
+        s_city = train[train['Embarked'] =='S'].shape[0]  # 스칼라
+        c_city = train[train['Embarked'] =='C'].shape[0]  # 스칼라
+        q_city = train[train['Embarked'] =='Q'].shape[0]  # 스칼라
 
+        print("S 에서 승선한 탑승객 수: {}".format(s_city))
+        print("C 에서 승선한 탑승객 수: {}".format(c_city))
+        print("Q 에서 승선한 탑승객 수: {}".format(q_city))
+
+        train = train.fillna({"Embarked":"S"})
+        city_mapping = {"S" : 1, "C" : 2, "Q" : 3}
+        train['Embarked'] = train['Embarked'].map(city_mapping)
+        test['Embarked'] = test['Embarked'].map(city_mapping)
+        return [train, test]
 
 
 

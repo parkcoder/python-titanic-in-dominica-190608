3472ae6338c07838776b4f79825bbe9216d22f48
pakjkwan <pakjkwan@gmail.com>
2019-06-29 18:33:51 +0900

    final

---
+++ supervised_learning/__init__.py
@@ -0,0 +1,5 @@
+from supervised_learning.controlller import SupervisedLearningController
+
+if __name__ == '__main__':
+    ctrl = SupervisedLearningController()
+    ctrl.test()
---
+++ supervised_learning/controlller.py
@@ -0,0 +1,10 @@
+from supervised_learning.ram_price import RamPrice
+
+class SupervisedLearningController:
+
+    def __init__(self):
+        self._m = RamPrice()
+
+    def test(self):
+        m = self._m
+        m.hook()
